b15210df9f7dadf87b260b885caf76df07d09b59
jungyu.Jenog <ome20@naver.com>
2020-05-06 15:08:28 +0900

    test.java 2줄 주석

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test{
 	public static void (String [] args){
 //		System.out.println("Hello");
-		System.out.println("Git");
+//		System.out.println("Git");
 		System.out.println("Hi");
 		System.out.println("branch1");
 		System.out.println("branch2");
